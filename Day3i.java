69cdfd4086f9c4be370fd78548fd37b7b1132bca
Amar13mehta <57030863+Amar13mehta@users.noreply.github.com>
2020-08-27 18:43:46 +0530

    Scanner.hasNext(),while loop

---
+++ Day3i.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 1;
+        while(sc.hasNext())
+        {
+           String a = sc.nextLine();
+           System.out.println(n+" "+a);
+           n++;
+
+        }
+    }
+
+
+}
