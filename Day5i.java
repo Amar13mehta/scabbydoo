646341f1efde4196ca8e5d55f0e2669c3dc2e565
Amar13mehta <57030863+Amar13mehta@users.noreply.github.com>
2020-08-30 12:46:03 +0530

    getCurrencyInstance Method, NumberFormat

---
+++ Day5i.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+ NumberFormat nf1 = NumberFormat.getCurrencyInstance(Locale.US);
+ String us = nf1.format(payment);
+
+ NumberFormat nf2 = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+ String india = nf2.format(payment);
+
+ NumberFormat nf3 = NumberFormat.getCurrencyInstance(Locale.CHINA);
+ String china = nf3.format(payment);
+
+ NumberFormat nf4= NumberFormat.getCurrencyInstance(Locale.FRANCE);
+ String france = nf4.format(payment);
+        
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
