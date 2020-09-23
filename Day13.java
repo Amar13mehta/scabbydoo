cb30d119f0e165c5badc74ec31f0db48ce42e23e
Amar13mehta <57030863+Amar13mehta@users.noreply.github.com>
2020-09-23 16:38:00 +0530

    sum of array

---
+++ Day13.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class A {
+
+ 
+     public static void main(String[] args) {
+     
+     Scanner s = new Scanner(System.in);
+         int A = s.nextInt();
+          int[] ar = new int[A];
+         int B;
+         for( int t=0;t<A;t++)
+         {
+               B = s.nextInt();
+               ar[t]=B;
+         }
+          int sum=0;
+         for( int i=0;i<A;i++)
+         {
+         sum = sum+ar[i];
+         }
+         System.out.println(""+sum);
+     }
+}
+
+    
